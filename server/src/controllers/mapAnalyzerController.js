0fab073224a82f05f7985d782213524300f92ea4
robert2030ca <robert2030ca@gmail.com>
2019-10-23 20:02:25 -0400

    backend location data parse works

--- server/src/controllers/mapAnalyzerController.js
+++ server/src/controllers/mapAnalyzerController.js
@@ -17,12 +17,47 @@ function getData(req, res, next){
   console.log('in getData function');
   const locationDataFile = appRoot + 'myData/Location History.json';
 
-  console.log(locationDataFile);
-
   const locationDataFileContents = fs.readFileSync(locationDataFile);
   const locationData = JSON.parse(locationDataFileContents)
 
-  console.log(Object.keys(locationData.locations[1]));
+  //count the number of data points
+  let datapoints = Object.keys(locationData.locations).length;
+
+  //find the timestampe of all datapoints
+  let timeStampArray=[];
+  for (let i=0; i<datapoints; i++){
+    if (i!=0){
+       timeStampArray.push((locationData.locations[i].timestampMs-locationData.locations[i-1].timestampMs)/1000);
+    }
+  }
+
+  let parsedResult={
+    maxInterval: getMax(timeStampArray)/1000,
+    minInterval: getMin(timeStampArray)/1000,
+    dataIntervals:timeStampArray
+  };
+  console.log('complete, max time interval:', parsedResult.maxInterval)
+  res.json(parsedResult)
+}
+
+function getMax(arr) {
+    let len = arr.length;
+    let max = -Infinity;
+
+    while (len--) {
+        max = arr[len] > max ? arr[len] : max;
+    }
+    return max;
+}
+
+function getMin(arr) {
+    let len = arr.length;
+    let min = Infinity;
+
+    while (len--) {
+        min = arr[len] < min ? arr[len] : min;
+    }
+    return min;
 }
 
 module.exports = {
